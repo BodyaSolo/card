a2c6d457883544402df5c4c639bf79c473657f31
BodyaSolo <bodyasolo@gmail.com>
2022-04-03 21:36:07 +0300

    master

--- script.js
+++ script.js
@@ -16,3 +16,41 @@ document.querySelector('.slider-prev').addEventListener('click', function () {
     }
     sliderLine.style.left = -offset + 'px';
 });
+
+let offset2 = 0;
+const sliderLine2 = document.querySelector('.slider-line2');
+
+document.querySelector('.slider-next2').addEventListener('click', function(){
+    offset = offset + 200;
+    if (offset > 400) {
+        offset = 0;
+    }
+    sliderLine2.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev2').addEventListener('click', function () {
+    offset = offset - 200;
+    if (offset < 0) {
+        offset = 400;
+    }
+    sliderLine2.style.left = -offset + 'px';
+});
+
+let offset3 = 0;
+const sliderLine3 = document.querySelector('.slider-line3');
+
+document.querySelector('.slider-next3').addEventListener('click', function(){
+    offset = offset + 200;
+    if (offset > 400) {
+        offset = 0;
+    }
+    sliderLine3.style.left = -offset + 'px';
+});
+
+document.querySelector('.slider-prev3').addEventListener('click', function () {
+    offset = offset - 200;
+    if (offset < 0) {
+        offset = 400;
+    }
+    sliderLine3.style.left = -offset + 'px';
+});
